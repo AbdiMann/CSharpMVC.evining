3ef71af75cabc69fdee17e743bb69408068bfb07
AbdiMann <cabdulraxmann@gmail.com>
2016-12-30 21:54:36 +0300

    First Commit.

---
+++ MVCEvening/Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCEvening.ViewModels;
+
+
+namespace MVCEvening.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+
+        CustomerForm customerForm = new CustomerForm();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CustomerForm customerForm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public ActionResult CustomersList()
+        {
+            var myCustomerList = customerForm.GetPersonInfo();
+            return View(myCustomerList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CustomerForm customerForm)
+        {
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MVCEvening/ViewModels/CustomerForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCEvening.ViewModels
+{
+    public class CustomerForm
+    {
+        public int Id { get; set; }
+        [Required, Display(Name = "Customer Name")]
+        public string CustomerName { get; set; }
+        [Required]
+        public string CustomerAddress { get; set; }
+        [Required]
+        public string CustomerPhone { get; set; }
+        [Required]
+        public int Number { get; set; }
+
+
+
+
+        public List<CustomerForm> GetPersonInfo()
+        {
+            return new List<CustomerForm>
+            {
+                new CustomerForm {
+                    Id = 1,
+                    CustomerName = "ahmed",
+                    CustomerAddress =  "Hargeisa",
+                    CustomerPhone =  "123456"
+                },
+                 new CustomerForm {
+                    Id = 1,
+                    CustomerName = "ahmed",
+                    CustomerAddress =  "Hargeisa",
+                    CustomerPhone =  "123456"
+                },
+                  new CustomerForm {
+               Id = 1,
+                    CustomerName = "ahmed",
+                    CustomerAddress =  "Hargeisa",
+                    CustomerPhone =  "123456"
+                },
+
+                new CustomerForm {
+                    Id = 1,
+                    CustomerName = "ahmed",
+                    CustomerAddress =  "Hargeisa",
+                    CustomerPhone =  "123456"
+                },
+
+
+
+        };
+        }
+    }
+}
